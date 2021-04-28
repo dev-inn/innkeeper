23e563f0b6449b35fd8cf51f63b6aecee261620a
nullFoo <38643612+nullFoo@users.noreply.github.com>
2021-04-29 11:31:01 +1200

    commands dict will clean up code in the long run

--- bot.py
+++ bot.py
@@ -11,11 +11,16 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 # connect to database
 con = sqlite3.connect('reputation.db')
 
-client = discord.Client()
+client = discord.Client() # set up bot with discord api
+
+# Commands dict
+commands = {
+    "help":help
+}
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!') # just a debug message to let us know bot is up and running
 
 @client.event
 async def on_message(message):
@@ -25,9 +30,17 @@ async def on_message(message):
     if(not message.content.startswith(prefix)): # if the message doesn't start with the prefix, ignore
         return
 
-    if(message.content.startswith(prefix + "help")): # if the message is "[prefix]help", or [prefix]help with anything after it, run this command
-        embed=discord.Embed(title="Command list") # create an embed named "Command list"
-        embed.add_field(name="..help", value="Shows this embed", inline=False) # TODO: instead of writing these out by hand, have a dict of commands
-        await message.channel.send(embed=embed) # send the embed
+    try:
+        commands[message.replace(prefix,'').split(" ")[0]](message) # ugly line that runs the function corresponding to the command without the prefix
+        # but only the first word in the command to ignore args
+    except Exception as e:
+        await message.channel.send("Command not found!")
+
+
+def help(message) {
+    embed=discord.Embed(title="Command list") # create an embed named "Command list"
+    embed.add_field(name="..help", value="Shows this embed", inline=False) # TODO: instead of writing these out by hand, have a dict of commands
+    await message.channel.send(embed=embed) # send the embed
+}
 
 client.run(TOKEN) # run the bot

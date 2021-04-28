3670602519035b775128fb40eafaf844163e3ce6
nullFoo <38643612+nullFoo@users.noreply.github.com>
2021-04-29 11:17:59 +1200

    help command

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 import discord
 import sqlite3
 
+prefix = "?"
+
 # load secrets
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
@@ -14,5 +16,18 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+    if(not message.content.startswith(prefix)):
+        return
+
+    if(message.content.startswith(prefix + "help")):
+        embed=discord.Embed(title="Command list")
+        embed.add_field(name="..help", value="Shows this embed", inline=False)
+        await message.channel.send(embed=embed)
 
 client.run(TOKEN)

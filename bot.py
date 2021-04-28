14fcd35373f3089b78fe5bb65af78e1b7de0791d
Michael Verges <maustinstar@gmail.com>
2021-04-28 18:57:46 -0400

    remove dotenv in favor of github secrets

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 # bot.py
 import os
-
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+# load secrets
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
 client = discord.Client()
 

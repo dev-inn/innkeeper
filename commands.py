8d8d6b1672a9a77fbd7113ee135c9c83758b9606
Michael Verges <maustinstar@gmail.com>
2021-04-29 03:44:13 -0400

    Credit status

--- commands.py
+++ commands.py
@@ -97,7 +97,9 @@ async def award(message):
         return
 
     db.award(message.author.id, user.id)
-    await message.channel.send('Awarded 1 reputation to ' + user.mention + '.')
+    await message.channel.send('Awarded 1 reputation to ' + user.mention + '.' +
+        message.author.mention ' has ' + db.getCredits(message.author.id) +
+        ' remaining credits.')
 
 async def reputation(message):
     '''

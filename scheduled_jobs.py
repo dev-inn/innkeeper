aece7a636e0252efbdadd0e8581220496730be0f
Michael Verges <maustinstar@gmail.com>
2021-04-29 15:52:01 -0400

    Hook balance reload into rank registry

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -5,17 +5,17 @@ import time
 
 # local imports
 import database as db
+import ranks
 
 def reload_awards():
     '''
     Reloads award budget every 6 hours
     '''
-    ranks = [1, 2, 3, 4, 5] # TODO: hook into ranks system
 
     # Reload a budget equal to user rank
     # e.g. 1 award budget per 1 rank every 6 hrs
-    for i in ranks:
-        db.reload_awards(i, i)
+    for rank in rank_registry:
+        db.reload_awards(rank.rank, rank.budget)
 
 ###--------------------------------------------------------------------------###
 ### Add the Scheduler                                                        ###

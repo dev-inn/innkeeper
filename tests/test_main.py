92939ae7d9d5fe27d2b2ae161e7666a07503e4ae
Adam Hodgkinson <hodgkinson.adam@gmail.com>
2021-04-30 22:11:30 +0100

    basic testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,9 +5,24 @@ bot = Botdata()
 
 db = Database.DB(bot)
 
-db.nuke_db()
 
-database = db.cursor.execute("SELECT * FROM reputation").fetchall()
-assert database is None
+def test_reset_db():
+    reset_db()
+    database = db.cursor.execute("SELECT * FROM reputation").fetchall()
+    assert len(database) == 0
 
-db.register(72)
+
+def reset_db():
+    db.nuke_db()
+    db.start_userdb()
+    db.start_rankdb()
+
+
+def test_register():
+    db.register(72)
+    result = db.cursor.execute("SELECT value,rank,credits FROM reputation").fetchall()
+    assert len(result) == 1
+    entry = result[0]
+    assert entry[0] == 0  # rep ==0
+    assert entry[1] == 1  # rank == 1
+    assert entry[2] == 1  # credits == 1

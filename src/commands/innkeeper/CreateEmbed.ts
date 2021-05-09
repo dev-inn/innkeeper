fe0aebd7f0f5bde4b01228f34af04082d5017d15
August Emmery-Funch <32144571+mlohnrob@users.noreply.github.com>
2021-05-10 01:24:07 +0200

    add color option to createembed
    I'm not sure if this needs to be another command like the setembeddescription and setembedtitle commands...
    But at least this is something:)

--- src/commands/innkeeper/CreateEmbed.ts
+++ src/commands/innkeeper/CreateEmbed.ts
@@ -1,10 +1,34 @@
 import Command from '../../Command'
 import Discord from 'discord.js'
 
-const cmd = new Command('createembed', [], async (message, bot, args) => {
-    const embed = new Discord.MessageEmbed()
-    embed.setDescription('This is an embed message')
-    await message.channel.send(embed)
+const cmd = new Command(
+    'createembed',
+    [
+        { name: 'color'. optional: true }
+    ],
+    async (message, bot, args) => {
+        let argColor;
+        let color;
+        try {
+            argColor = args.color;
+        } catch (e) {
+            await message.reply("Error parsing arguments");
+            return;
+        }
+        const regex = argColor.match(/^#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/);
+        if (regex) {
+            color = argColor;
+        } else {
+            await message.reply("Invalid hex");
+            return;
+        }
+        
+        const embed = new Discord.MessageEmbed()
+        embed.setDescription('This is an embed message')
+        if (color) {
+            embed.setColor(color);
+        }
+        await message.channel.send(embed)
 })
 
 cmd.guildOnly = true

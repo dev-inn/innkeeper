8ff644229cb9582b93ff6ef85928fd2f0069b8b9
Adam Hodgkinson <hodgkinson.adam@gmail.com>
2021-05-30 22:45:11 +0100

    can now actually play audio from youtube

---
+++ src/commands/innkeeper/Play.ts
@@ -0,0 +1,44 @@
+import Command from '../../Command'
+import 'fs'
+import * as fs from 'fs'
+
+import { logger } from '@noodlewrecker7/logger'
+import ytdl from 'ytdl-core-discord'
+import ytsr, { Item, Video } from 'ytsr'
+
+const log = logger.Logger
+
+const insaneRegexString = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/
+
+const cmd = new Command(
+    'play',
+    [{ name: 'video', optional: false, catchRest: true }],
+    async (message, bot, args) => {
+        let url
+
+        // tests if input was a url
+        const regMatch = args.video.match(insaneRegexString)
+        if (regMatch) {
+            url = regMatch[0]
+        } else {
+            // otherwise it searches for a video by that name and gives first result
+            const results = await ytsr(args.video, { limit: 1 })
+            const item = <Video>results.items[0]
+            url = item.url
+        }
+        log.debug(`Playing ${url}`)
+        if (message.member?.voice?.channel) {
+            const connection = await message.member.voice.channel.join()
+            const dispatcher = connection.play(await ytdl(url), {
+                type: 'opus'
+            })
+        } else {
+            await message.reply('You are not in a voice channel')
+        }
+    }
+)
+cmd.guildOnly = true
+cmd.cooldown = 5000
+cmd.description = 'Plays a video'
+
+export default cmd
